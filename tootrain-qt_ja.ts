3f85b2091e9124a49d8402ac0525a2d58aeae5a0
b123400 <i@b123400.net>
2024-09-07 23:23:11 +0900

    Localise

---
+++ tootrain-qt_ja.ts
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ja">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.cpp" line="33"/>
+        <source>File</source>
+        <translation type="unfinished">ファイル</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="34"/>
+        <source>Preferences</source>
+        <translation type="unfinished">設定</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="59"/>
+        <source>Preferences...</source>
+        <translation type="unfinished">設定...</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="62"/>
+        <source>Quit</source>
+        <translation type="unfinished">終了</translation>
+    </message>
+</context>
+<context>
+    <name>MastodonOauthWindow</name>
+    <message>
+        <location filename="mastodonoauthwindow.ui" line="20"/>
+        <source>Login to Mastodon</source>
+        <translation type="unfinished">マストドンにログイン</translation>
+    </message>
+    <message>
+        <location filename="mastodonoauthwindow.ui" line="51"/>
+        <source>Host:</source>
+        <translation type="unfinished">ホスト：</translation>
+    </message>
+    <message>
+        <location filename="mastodonoauthwindow.ui" line="64"/>
+        <location filename="mastodonoauthwindow.ui" line="67"/>
+        <source>https://</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingWindow</name>
+    <message>
+        <location filename="settingwindow.ui" line="14"/>
+        <source>Preferences</source>
+        <translation type="unfinished">設定</translation>
+    </message>
+    <message>
+        <location filename="settingwindow.ui" line="26"/>
+        <location filename="settingwindow.cpp" line="53"/>
+        <source>Login</source>
+        <translation type="unfinished">ログイン</translation>
+    </message>
+    <message>
+        <location filename="settingwindow.ui" line="39"/>
+        <location filename="settingwindow.ui" line="88"/>
+        <source>TextLabel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingwindow.ui" line="52"/>
+        <source>Test get profile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingwindow.ui" line="75"/>
+        <source>Check for Update</source>
+        <translation type="unfinished">更新の確認</translation>
+    </message>
+    <message>
+        <location filename="settingwindow.cpp" line="46"/>
+        <source>Account: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="settingwindow.cpp" line="47"/>
+        <location filename="settingwindow.cpp" line="48"/>
+        <source>Logout</source>
+        <translation type="unfinished">ログアウト</translation>
+    </message>
+    <message>
+        <location filename="settingwindow.cpp" line="52"/>
+        <source>Not logged in</source>
+        <translation type="unfinished">ログインしてない</translation>
+    </message>
+    <message>
+        <location filename="settingwindow.cpp" line="147"/>
+        <source>Checking for update...</source>
+        <translation type="unfinished">更新を確認中</translation>
+    </message>
+    <message>
+        <location filename="settingwindow.cpp" line="165"/>
+        <source>New version available</source>
+        <translation type="unfinished">新しいバージョンに更新できます</translation>
+    </message>
+    <message>
+        <location filename="settingwindow.cpp" line="166"/>
+        <source>Click here to update</source>
+        <translation type="unfinished">新しいバージョンに更新する</translation>
+    </message>
+    <message>
+        <location filename="settingwindow.cpp" line="168"/>
+        <source>No update available</source>
+        <translation type="unfinished">すでに最新のバージョンです</translation>
+    </message>
+    <message>
+        <location filename="settingwindow.cpp" line="205"/>
+        <source>Updating...</source>
+        <translation type="unfinished">更新中...</translation>
+    </message>
+    <message>
+        <location filename="settingwindow.cpp" line="217"/>
+        <source>Restarting...</source>
+        <translation type="unfinished">再起動中</translation>
+    </message>
+</context>
+</TS>
