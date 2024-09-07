383d21d2050c5fc3a6510c47d95a62aa9ec1cea7
b123400 <i@b123400.net>
2024-09-07 23:23:35 +0900

    Try to switch to ja

--- main.cpp
+++ main.cpp
@@ -5,12 +5,30 @@
 #include <QPropertyAnimation>
 #include <QGraphicsDropShadowEffect>
 #include <QScreen>
+#include <QTranslator>
 
 #include "mastodonoauthwindow.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QTranslator translator;
+
+    QString langPath;
+#if defined(Q_OS_WIN)
+    langPath = "./tootrain-qt_ja.qm";
+#elif defined(Q_OS_MAC)
+    langPath = "../Resources/tootrain-qt_ja.qm";
+#else
+    langPath = "./tootrain-qt_ja.qm";
+#endif
+    if (translator.load(QLocale(), langPath)) {
+        qDebug() << "Installing translator";
+        QCoreApplication::installTranslator(&translator);
+    } else {
+        qDebug() << "Lang not found!";
+    }
+
     MainWindow w;
 
     w.show();
