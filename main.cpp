1ecb78c2ab6af8d49356481bb3947bf4a2bf9b78
b123400 <i@b123400.net>
2024-09-08 23:59:51 +0900

    Detect system language

--- main.cpp
+++ main.cpp
@@ -6,27 +6,32 @@
 #include <QGraphicsDropShadowEffect>
 #include <QScreen>
 #include <QTranslator>
-
-#include "mastodonoauthwindow.h"
+#include <QLocale>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     QTranslator translator;
 
-    QString langPath;
-#if defined(Q_OS_WIN)
-    langPath = "./tootrain-qt_ja.qm";
-#elif defined(Q_OS_MAC)
-    langPath = "../Resources/tootrain-qt_ja.qm";
-#else
-    langPath = "./tootrain-qt_ja.qm";
-#endif
-    if (translator.load(QLocale(), langPath)) {
-        qDebug() << "Installing translator";
-        QCoreApplication::installTranslator(&translator);
-    } else {
-        qDebug() << "Lang not found!";
+    QLocale sysLocale = QLocale::system();
+    QString localeName = sysLocale.name();
+    bool isJa = localeName.startsWith("ja");
+
+    if (isJa) {
+        QString langPath;
+    #if defined(Q_OS_WIN)
+        langPath = "./tootrain-qt_ja.qm";
+    #elif defined(Q_OS_MAC)
+        langPath = "../Resources/tootrain-qt_ja.qm";
+    #else
+        langPath = "./tootrain-qt_ja.qm";
+    #endif
+        if (translator.load(QLocale(), langPath)) {
+            qDebug() << "Installing translator";
+            QCoreApplication::installTranslator(&translator);
+        } else {
+            qDebug() << "Lang not found!";
+        }
     }
 
     MainWindow w;
