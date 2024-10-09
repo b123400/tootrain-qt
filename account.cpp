401bde7576844842d276c6beb67388648da9dbc2
b123400 <i@b123400.net>
2024-10-09 23:26:39 +0900

    Add channel to account model

--- account.cpp
+++ account.cpp
@@ -3,3 +3,21 @@
 Account::Account(QObject *parent)
     : QObject{parent}
 {}
+
+Account::Account(QSettings *settings, QObject *parent)
+    : QObject{parent}
+{
+    this->uuid = settings->value("uuid").toString();
+    this->displayName = settings->value("displayName").toString();
+    this->username = settings->value("username").toString();
+    this->id = settings->value("id").toString();
+    this->avatarUrl= settings->value("avatarUrl").toString();
+}
+
+void Account::saveToSettings(QSettings *settings) {
+    settings->setValue("uuid", this->uuid);
+    settings->setValue("displayName", this->displayName);
+    settings->setValue("username", this->username);
+    settings->setValue("id", this->id);
+    settings->setValue("avatarUrl", this->avatarUrl);
+}
--- mastodon/account.cpp
+++ mastodon/account.cpp
@@ -15,6 +15,37 @@ MastodonAccount::MastodonAccount(QJsonObject json, QObject *parent) : Account {p
     id = json["id"].toString();
 }
 
+MastodonAccount::MastodonAccount(QSettings *settings, QObject *parent) : Account(settings, parent) {
+    MastodonApp *app = new MastodonApp(this);
+    this->app = app;
+    QOAuth2AuthorizationCodeFlow *oauth2 = new QOAuth2AuthorizationCodeFlow(app);
+    app->oauth2 = oauth2;
+    app->clientId = settings->value("clientId").toString();
+    oauth2->setClientIdentifier(app->clientId);
+    oauth2->setToken(settings->value("accessToken").toString());
+    app->clientSecret = settings->value("clientSecret").toString();
+    app->redirectUri = settings->value("redirectUri").toString();
+    app->baseUrl = settings->value("baseUrl").toUrl();
+
+    if (settings->contains("source")) {
+        bool sourceOk = false;
+        int source_int = settings->value("source").toInt(&sourceOk);
+        if (!sourceOk) {
+            this->source = Source::User;
+        } else {
+            this->source = static_cast<Source>(source_int);
+        }
+        hashtag = settings->value("hashtag").toString();
+        listId = settings->value("listId").toString();
+        listName = settings->value("listName").toString();
+    } else {
+        source = Source::User;
+        hashtag = "";
+        listId = "";
+        listName = "";
+    }
+}
+
 QUrl MastodonAccount::getWebSocketUrl() {
     auto accessToken = this->app->oauth2->token();
     qDebug() << "access token: " << accessToken;
@@ -35,4 +66,19 @@ QString MastodonAccount::fullUsername() {
         return username;
     }
     return username + "@" + this->app->baseUrl.host();
+}
+
+void MastodonAccount::saveToSettings(QSettings *settings) {
+    this->Account::saveToSettings(settings);
+    settings->setValue("type", "mastodon");
+    settings->setValue("clientId", this->app->clientId);
+    settings->setValue("clientSecret", this->app->clientSecret);
+    settings->setValue("redirectUri", this->app->redirectUri);
+    settings->setValue("baseUrl", this->app->baseUrl);
+    settings->setValue("accessToken", this->app->oauth2->token());
+
+    settings->setValue("source", static_cast<int>(this->source));
+    settings->setValue("hashtag", this->hashtag);
+    settings->setValue("listId", this->listId);
+    settings->setValue("listName", this->listName);
 }
