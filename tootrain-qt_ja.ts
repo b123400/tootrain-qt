9f6ad7604074031ade35f54af106748cd60fe6fd
b123400 <i@b123400.net>
2024-11-07 15:36:07 +0900

    Localisation

--- tootrain-qt_ja.ts
+++ tootrain-qt_ja.ts
@@ -53,7 +53,7 @@
         <location filename="src/mainwindow.cpp" line="38"/>
         <location filename="src/mainwindow.cpp" line="67"/>
         <source>About...</source>
-        <translation>TootRainについて</translation>
+        <translation>TootRainについて...</translation>
     </message>
     <message>
         <location filename="src/mainwindow.cpp" line="71"/>
@@ -220,9 +220,13 @@
         <translation>ホスト：</translation>
     </message>
     <message>
-        <location filename="src/misskey/misskeyauthwindow.cpp" line="22"/>
         <source>Authorised, please go back to TootRain</source>
-        <translation>認証完了しました、TootRainに戻ってください</translation>
+        <translation type="vanished">認証完了しました、TootRainに戻ってください</translation>
+    </message>
+    <message>
+        <location filename="src/misskey/misskeyauthwindow.cpp" line="22"/>
+        <source>&lt;!DOCTYPE html&gt;&lt;html lang=&quot;en&quot;&gt;&lt;head&gt;&lt;meta charset=&quot;utf-8&quot;&gt;&lt;/head&gt;&lt;body&gt;Authorised, please go back to TootRain</source>
+        <translation>&lt;!DOCTYPE html&gt;&lt;html lang=&quot;ja&quot;&gt;&lt;head&gt;&lt;meta charset=&quot;utf-8&quot;&gt;&lt;/head&gt;&lt;body&gt;認証完了しました、TootRainに戻ってください</translation>
     </message>
 </context>
 <context>
@@ -235,7 +239,7 @@
     <message>
         <location filename="src/misskey/misskeysettingwindow.ui" line="48"/>
         <source>Please see &lt;a href=&quot;https://misskey-hub.net/ja/docs/for-developers/api/streaming/channel/&quot;&gt;here&lt;/a&gt; for more info.</source>
-        <translation>&lt;a href=&quot;https://misskey-hub.net/ja/docs/for-developers/api/streaming/channel/&quot;&gt;もっと詳しく&lt;/a&gt;.</translation>
+        <translation>&lt;a href=&quot;https://misskey-hub.net/ja/docs/for-developers/api/streaming/channel/&quot;&gt;もっと詳しく&lt;/a&gt;</translation>
     </message>
 </context>
 <context>
@@ -284,84 +288,99 @@
 <context>
     <name>SettingWindow</name>
     <message>
-        <location filename="src/settingwindow.ui" line="32"/>
+        <location filename="src/settingwindow.ui" line="26"/>
         <source>Preferences</source>
         <translation>設定</translation>
     </message>
     <message>
-        <location filename="src/settingwindow.ui" line="44"/>
-        <location filename="src/settingwindow.cpp" line="60"/>
+        <location filename="src/settingwindow.ui" line="38"/>
+        <source>Account</source>
+        <translation>アカウント</translation>
+    </message>
+    <message>
+        <location filename="src/settingwindow.ui" line="50"/>
+        <location filename="src/settingwindow.cpp" line="57"/>
         <source>Login</source>
         <translation>ログイン</translation>
     </message>
     <message>
-        <location filename="src/settingwindow.ui" line="57"/>
-        <source>TextLabel</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="src/settingwindow.ui" line="70"/>
-        <source>Test get profile</source>
-        <translation></translation>
-    </message>
-    <message>
-        <location filename="src/settingwindow.ui" line="93"/>
+        <location filename="src/settingwindow.ui" line="138"/>
         <source>Check for Update</source>
         <translation>更新の確認</translation>
     </message>
     <message>
-        <location filename="src/settingwindow.ui" line="119"/>
+        <location filename="src/settingwindow.ui" line="76"/>
         <source>Configure...</source>
         <translation>チャンネル設定...</translation>
     </message>
     <message>
-        <location filename="src/settingwindow.ui" line="132"/>
+        <location filename="src/settingwindow.ui" line="63"/>
+        <source>Status</source>
+        <translation>ログイン状態</translation>
+    </message>
+    <message>
+        <location filename="src/settingwindow.ui" line="90"/>
+        <source>Screen</source>
+        <translation>スクリーン</translation>
+    </message>
+    <message>
+        <location filename="src/settingwindow.ui" line="113"/>
+        <source>Updates</source>
+        <translation>アップデート</translation>
+    </message>
+    <message>
+        <location filename="src/settingwindow.ui" line="152"/>
+        <source>Appearance</source>
+        <translation>見た目</translation>
+    </message>
+    <message>
+        <location filename="src/settingwindow.ui" line="164"/>
         <source>Show avatar</source>
         <translation>ユーザーアイコンを表示</translation>
     </message>
     <message>
-        <location filename="src/settingwindow.cpp" line="53"/>
+        <location filename="src/settingwindow.cpp" line="50"/>
         <source>Account: </source>
         <translation>アカウント：</translation>
     </message>
     <message>
-        <location filename="src/settingwindow.cpp" line="54"/>
+        <location filename="src/settingwindow.cpp" line="51"/>
         <source>Logout</source>
         <translation>ログアウト</translation>
     </message>
     <message>
-        <location filename="src/settingwindow.cpp" line="59"/>
+        <location filename="src/settingwindow.cpp" line="56"/>
         <source>Not logged in</source>
         <translation>ログインしてない</translation>
     </message>
     <message>
-        <location filename="src/settingwindow.ui" line="106"/>
-        <location filename="src/settingwindow.cpp" line="255"/>
+        <location filename="src/settingwindow.ui" line="125"/>
+        <location filename="src/settingwindow.cpp" line="243"/>
         <source>Checking for update...</source>
         <translation>更新を確認中</translation>
     </message>
     <message>
-        <location filename="src/settingwindow.cpp" line="273"/>
+        <location filename="src/settingwindow.cpp" line="261"/>
         <source>New version available</source>
         <translation>新しいバージョンに更新できます</translation>
     </message>
     <message>
-        <location filename="src/settingwindow.cpp" line="274"/>
+        <location filename="src/settingwindow.cpp" line="262"/>
         <source>Click here to update</source>
         <translation>新しいバージョンに更新する</translation>
     </message>
     <message>
-        <location filename="src/settingwindow.cpp" line="276"/>
+        <location filename="src/settingwindow.cpp" line="264"/>
         <source>No update available</source>
         <translation>すでに最新のバージョンです</translation>
     </message>
     <message>
-        <location filename="src/settingwindow.cpp" line="313"/>
+        <location filename="src/settingwindow.cpp" line="301"/>
         <source>Updating...</source>
         <translation>更新中...</translation>
     </message>
     <message>
-        <location filename="src/settingwindow.cpp" line="324"/>
+        <location filename="src/settingwindow.cpp" line="312"/>
         <source>Restarting...</source>
         <translation>再起動中</translation>
     </message>
